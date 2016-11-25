41665939bd8d8bb2c4348469cd59ac1432d3691e
opsb <oliver@opsb.co.uk>
2016-11-25 09:49:42 +0100

    Upgrade phoenix dependencies

--- example/mix.exs
+++ example/mix.exs
@@ -30,10 +30,10 @@ defmodule ElmPhoenix.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.1.6"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_ecto, "~> 2.0"},
-     {:phoenix_html, "~> 2.4"},
+    [{:phoenix, "~> 1.2.0"},
+     {:postgrex, "~> 0.12"},
+     {:phoenix_ecto, "~> 3.0"},
+     {:phoenix_html, "~> 2.8"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"}]
